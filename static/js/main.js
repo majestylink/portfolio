a6160c96490c07071c81d4f909aaf953b8596fc6
Atuh Samuel <atuhsamuel96@gmail.com>
2023-05-25 10:36:04 +0100

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,237 +1,117 @@
-// 
+(function ($) {
+    "use strict";
 
-(function($,sr) {
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  var debounce = function (func, threshold, execAsap) {
-    var timeout;
-    return function debounced () {
-      var obj = this, args = arguments;
-      function delayed () {
-        if (!execAsap)
-                          func.apply(obj, args);
-        timeout = null;
-      }
-      ;
-      if (timeout)
-                    clearTimeout(timeout); else if (execAsap)
-                    func.apply(obj, args);
-      timeout = setTimeout(delayed, threshold || 100);
-    }
-    ;
-  }
-  // smartresize 
-  jQuery.fn[sr] = function(fn) {
-    return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
-  }
-  ;
-}
-)(jQuery,'smartresize');
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
 
-$(function() {
 
-    // Fix the Home Height
-
-    var setHomeBannerHeight = function(){
-	   var homeHeight= $(window).height();
-	   $('#overlay-1').height(homeHeight);
-    }
-    setHomeBannerHeight();
-
-    // Arrow drop effect
-
-    var $scrollDownArrow = $('.bottom > a');
-
-    // Smooth Scrolling and remove Hash tag from link
-
-    $('a[href*=#]:not([href=#])').click(function() {
-       
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function(){
-                  
-                });
-                return false;
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
             }
         }
     });
 
 
-  ///////////////////////////////
-  // Center Home Slideshow Text
-  ///////////////////////////////
-  function centerHomeBannerText() {
-    var bannerText = jQuery('#header .middle');
-    var bannerTextTop = (jQuery('#header').actual('height')/2) - (jQuery('#header .middle').actual('height')/2) - 20;
-    bannerText.css('padding-top', bannerTextTop+'px');
-    bannerText.show();
-  }
-  centerHomeBannerText();
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
 
 
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
 
-    jQuery(window).smartresize(function() {
-        setHomeBannerHeight();
-        centerHomeBannerText();
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
     
-});
-
-
-$( function() {
-  // init Isotope
-  var $container = $('.isotope').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-      name: '.name',
-      symbol: '.symbol',
-      number: '.number parseInt',
-      category: '[data-category]',
-      weight: function( itemElem ) {
-        var weight = $( itemElem ).find('.weight').text();
-        return parseFloat( weight.replace( /[\(\)]/g, '') );
-      }
-    }
-  });
-
-  // filter functions
-  var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-      var number = $(this).find('.number').text();
-      return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-      var name = $(this).find('.name').text();
-      return name.match( /ium$/ );
-    }
-  };
-
-  // bind filter button click
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $container.isotope({ filter: filterValue });
-  });
-
-  // bind sort button click
-  $('#sorts').on( 'click', 'button', function() {
-    var sortByValue = $(this).attr('data-sort-by');
-    $container.isotope({ sortBy: sortByValue });
-  });
-  
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
-  });
-
-  
-});
-
-function countUpTo(count,selector,max)
-    {
-      console.log("count--> "+count);
-        var div_by = count,
-            speed = Math.round(count / div_by),
-            $display = selector,
-            run_count = 1,
-            int_speed = 24;
-
-        var int = setInterval(function() {
-            if(run_count < div_by){
-                $display.text(speed * run_count);
-                run_count++;
-            } else if(parseInt($display.text()) < count) {
-                var curr_count = parseInt($display.text()) + 1;
-                var text = "";
-                if(max>99){
-                     if(curr_count<10){
-                        text = text+"00"+curr_count;
-                    }
-                    /*else if(curr_count < 100 && curr_count >9){
-                        text = text+"0"+curr_count;
-                    }*/
-                    else{
-                      text = curr_count;
-                    }
-                }else if(max<100 && max>9){
-                     if(curr_count<10){
-                        text = text+"00"+curr_count;
-                    }
-                   /*else if(curr_count < 100 && curr_count >9){
-                        text = text+"0"+curr_count;
-                    }*/
-                    else{
-                      text = curr_count;
-                    }
-                }else{
-                      if(curr_count<10){
-                        text = text+"00"+curr_count;
-                    }
-                   /*else if(curr_count < 100 && curr_count >9){
-                        text = text+"0"+curr_count;
-                    }*/
-                    else{
-                      text = curr_count;
-                    }
-                }
-               
-                $display.text(text);
-            } else {
-                clearInterval(int);
-            }
-        }, int_speed);
-    }
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
 
 
-var firstTime = true;
-$(document).scroll(function(event) {
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
 
-
-
-  var result = $('.count-timer').isOnScreen();
-
-  if(result == true) {
-      console.log("on screen");
-
-      if(firstTime){
-        firstTime = false;
-            
-          var count1 = $('.count1'),
-            count2 = $('.count2'),
-            count3 = $('.count3'),
-            count4 = $('.count4'),
-            count5 = $('.count5'),
-            count6 = $('.count6')
-            count1Num = count1.text(),
-            count2Num = count2.text(),
-            count3Num = count3.text(),
-            count4Num = count4.text(),
-            count5Num = count5.text(),
-            count6Num = count6.text();
-
-            var max = Math.max(parseInt(count1Num),parseInt(count2Num));
-            max = Math.max(max,parseInt(count6Num));
-            console.log(max);
-
-            countUpTo(count1Num,count1,max);
-            countUpTo(count2Num,count2,max);
-            countUpTo(count3Num,count3,max);
-            countUpTo(count4Num,count4,max);
-            countUpTo(count5Num,count5,max);
-            countUpTo(count6Num,count6,max);
-      }
-
-    }
-});
